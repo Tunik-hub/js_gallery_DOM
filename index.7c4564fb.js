cd3506e20670864926a5a7146c8e770b9ce46d3e
Vadym Bakuma <bakuma.2001@gmail.com>
2024-06-28 22:50:33 +0200

    make build

---
+++ index.7c4564fb.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelectorAll(".gallery__list a").forEach(function(t){t.addEventListener("click",function(r){r.preventDefault();var l=t.getAttribute("href");e.src=l})});
+//# sourceMappingURL=index.7c4564fb.js.map
